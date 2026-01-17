9ddc1e15337b33545ce47e9cf730d77d84ae339e
klkeehan <klkeehan@gmail.com>
2026-01-17 18:59:38 -0500

    1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>preserv</h1>
         <p>test</p>
       </header>
