90a920afc0e5202a6f72e49763618ce4b2db52c9
klkeehan <klkeehan@gmail.com>
2026-02-20 15:26:31 -0500

    php file

--- db_connect.php
+++ db_connect.php
@@ -1,18 +1,8 @@
 <?php
-  // loading data from .env file **DO NOT REMOVE**
-  require_once realpath(__DIR__ . "/vendor/autoload.php");
-  use Dotenv\Dotenv;
-  $dotenv = Dotenv::createImmutable(__DIR__);
-  $dotenv->load();
-
-  $user = $_ENV['MYSQL_USER'];
-  $pw = $_ENV['MYSQL_PASSWORD'];
-
-  $mysqli = new mysqli("localhost", $user, $pw, "preserv");
+$mysqli = new mysqli("127.0.0.1", "syWCdQdRwM", "wsacrLpDPg", "dbsyWCdQdRwM");
   if ($mysqli->error) {
     print "Error connecting! Message:" . $mysqli->error;
   }
   else {
     print "hello ";
-    echo $user;
   }
