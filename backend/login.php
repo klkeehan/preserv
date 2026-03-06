77ce072e5eca063956fab39b1e536b4aa73329e6
klkeehan <klkeehan@gmail.com>
2026-03-06 18:46:42 -0500

    login backend

--- backend/login.php
+++ backend/login.php
@@ -10,13 +10,15 @@
   include('connect.php');
   if (isset($_SERVER['HTTP_ORIGIN'])) {
     $http_origin = $_SERVER['HTTP_ORIGIN'];
-    if ($http_origin == "http://localhost:3000" || $http_origin == "http://localhost:8080" || $http_origin == "http://preserv-one.vercel.app/") {
+    if ($http_origin == "http://localhost:3000" || $http_origin == "http://localhost:8080") {
       header("Access-Control-Allow-Origin: $http_origin");
       header("Access-Control-Allow-Credentials: true");
     }
   }
+  header('Access-Control-Allow-Origin: https://preserv-one.vercel.app/');
+  header('Access-Control-Allow-Credentials: true');
   header('Access-Control-Allow-Headers: Content-Type');
-  header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+  header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS');
   header('Content-Type: application/json; charset=UTF-8');
 
   $method = $_SERVER['REQUEST_METHOD'];
