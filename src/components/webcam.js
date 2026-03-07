f15e9ca339fd93b0b5e7985480c6793c38f702fd
klkeehan <klkeehan@gmail.com>
2026-03-07 17:05:22 -0500

    scan

--- src/components/webcam.js
+++ src/components/webcam.js
@@ -1,37 +1,43 @@
+import Quagga from '@ericblade/quagga2';
+
 const WebcamCapture = () => {
   const Quagga = require('@ericblade/quagga2').default;
-  Quagga.init({
-    inputStream: {
-      type: 'LiveStream',
-      target: document.querySelector('#camera'),
-      constraints: {
-        width: 640,
-        height: 480,
-        facingMode: 'environment'
+  function startScanner() {
+    let scanFlag = false;
+    Quagga.init({
+      inputStream: {
+        name: 'Live',
+        type: 'LiveStream',
+        target: document.querySelector('#camera'),
+        constraints: {
+          width: 640,
+          height: 480,
+          facingMode: 'environment'
+        }
+      },
+      decoder: {
+        readers: ['upc_reader']
       }
-    },
-    decoder: {
-      readers: ['upc_reader']
-    }
-  }, function(err) {
-    if (err) {
-      console.log(err);
-      return;
-    };
-    console.log('initializing quagga . . .');
-    Quagga.start();
-    console.log('starting quagga . . .');
-  });
+    }, function(err) {
+      if (err) {
+        console.log(err);
+        return;
+      };
+      console.log('initializing quagga . . .');
+      Quagga.start();
+      console.log('starting quagga . . .');
+    });
 
-  Quagga.onDetected(function(result) {
-    const code = result.codeResult.code;
-    console.log('found code ', code);
-  });
+    Quagga.onDetected(function(result) {
+      const code = result.codeResult.code;
+      console.log('found code ', code);
+    });
+  };
 
   return (
     <div>
+      <script src='https://cdn.rawgit.com/serratus/quaggaJS/0420d5e0/dist/quagga.min.js'></script>
       <div id='camera' className='cam' style={{width: '100%'}}></div>
-      <script src='<script src="https://cdn.jsdelivr.net/npm/@ericblade/quagga2/dist/quagga.js'></script>
     </div>
   );
 };
